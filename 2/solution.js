63728488f7715652e8d5be32ec85f2a96b1c31e3
VRaitzev <163679361+VRaitzev@users.noreply.github.com>
2024-11-08 20:00:35 +0300

    Update solution.js

--- 2/solution.js
+++ 2/solution.js
@@ -8,5 +8,21 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const sql = postgres(config);
 
+export default async function solution(articles) {
+  const result = [];
+
+  for (const article of articles) {
+    const { title, description } = article;
+    const res = await sql`
+      INSERT INTO articles (title, description)
+      VALUES (${title}, ${description})
+      RETURNING id
+    `;
+    result.push(res[0].id); 
+  }
+
+  return result; 
+}
 // END
