a80966b602da612d85c25ff2be73679a6a143272
VRaitzev <163679361+VRaitzev@users.noreply.github.com>
2024-11-08 20:00:00 +0300

    Update solution.js

--- 1/solution.js
+++ 1/solution.js
@@ -8,5 +8,30 @@ const config = {
 };
 
 // BEGIN (write your solution here)
+const sql = postgres(config)
 
+async function solution() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS articles (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255),
+        description VARCHAR(255)
+      );
+    `;
+
+    await sql`
+      INSERT INTO articles (title, description)
+      VALUES ('Пицца', 'Лучшее блюдо в мире');
+    `;
+
+    console.log("Таблица создана и запись добавлена.");
+  } catch (err) {
+    console.error("Ошибка при работе с базой данных:", err);
+  } finally {
+    await sql.end();
+  }
+}
+
+export default solution;
 // END
