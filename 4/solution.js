465a72bb15d513c2c8abd92166dcb5994cac09fb
VRaitzev <163679361+VRaitzev@users.noreply.github.com>
2024-11-08 20:01:32 +0300

    Update solution.js

--- 4/solution.js
+++ 4/solution.js
@@ -7,6 +7,45 @@ const config = {
   port: 5432,
 };
 
-// BEGIN (write your solution here)
+const sql = postgres(config);
 
-// END
+export default async function solution(client, { username, phone }, roomNumber, price) {
+
+  const transaction = await sql.begin();
+
+  try {
+    const userRes = await transaction`
+      INSERT INTO users (username, phone)
+      VALUES (${username}, ${phone})
+      RETURNING id
+    `;
+    const userId = userRes[0].id;
+
+    const roomRes = await transaction`
+      SELECT id, status FROM rooms WHERE room_number = ${roomNumber}
+    `;
+    const room = roomRes[0];
+    
+    if (room.status !== "free") {
+      throw new Error("Комната уже забронирована");
+    }
+
+    await transaction`
+      INSERT INTO orders (user_id, room_id, price)
+      VALUES (${userId}, ${room.id}, ${price})
+    `;
+
+    await transaction`
+      UPDATE rooms
+      SET status = 'reserved'
+      WHERE id = ${room.id}
+    `;
+
+    await transaction.commit();
+
+    return { userId, roomId: room.id };
+  } catch (error) {
+    await transaction.rollback();
+    throw error;  
+  }
+}
